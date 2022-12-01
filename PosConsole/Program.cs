75be189d8f1ec68cb3ad0041795a97b1a305f213
DESKTOP-5JMSEDB <yegorm2004@gmail.com>
2022-12-01 20:16:48 +0500

    initial commit

---
+++ PosConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = new HostBuilder()
+            .ConfigureServices(
+                (
+                    hostContext,
+                    services
+                ) =>
+                {
+                  //  services.AddHttpClient();
+                  
+                }
+            ).UseConsoleLifetime();
+        var host = builder.Build();
+
+        using var serviceScope = host.Services.CreateScope();
+        {
+            var services = serviceScope.ServiceProvider;
+            Console.WriteLine("staring postest v1.0");
+        }
+
+        return 0;
+    
